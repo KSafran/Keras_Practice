0ac54582c0d5d2b616847594b4ceb0395700281b
Kyle Safran <ksafran356@gmail.com>
2017-11-06 10:15:06 -0500

    add word index to embedding lookup

--- other_text/four_gram_model.py
+++ other_text/four_gram_model.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras.utils import to_categorical
 import pandas as pd
 from scipy.io import loadmat
-
+import numpy as np
 
 # Load data
 # this dataset comes form the coursera neural networks course
@@ -40,17 +40,21 @@ model.fit(trainData[:, [0,1,2]], target, epochs = 4, batch_size = 1000)
 
 # Now check accuracy on test data
 test_target = to_categorical(validData[:, -1], num_classes = 251)
-score = model.validate(x_test, y_test)
+score = model.evaluate(validData[:, :-1], test_target)
 print('''The out of sample cross entropy is %.3f and the out 
 	of sample accuracy is %.3f''' % (score[0], score[1]))
 
 # Can we see anything from our embedding layer?
 from keras import backend as K
 
+# This function returns the embedding output given a 3-gram
 get_embedding = K.function([model.layers[0].input],
                                   [model.layers[0].output])
 
-# shape isn't working here
-# the tensor input should have dimension [None, 3]
-# not sure what none means.
+# we need to reshape the input here to fit with the embedding layer
 layer_output = get_embedding([trainData[1,:-1].reshape([1,3])])
+
+# how about a function that takes a word index and returns the embedding
+def get_emb_index(ind):
+	embedding_output = get_embedding([np.array([ind, ind, ind], ndmin = 2)])
+	return(embedding_output[0][0,0,:])
